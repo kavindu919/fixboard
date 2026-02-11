1108b5304b89be4aa4be1ef61a3268544d4f9721
kavindu919 <jayakodyk2@gmail.com>
2026-02-11 23:09:11 +0530

    feat: create logout route

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   checkAuthentication,
   userLogin,
+  userLogout,
   userRegistration,
 } from "../controllers/authController";
 import { authMiddleware } from "../middleware/authMiddleware";
@@ -10,6 +11,7 @@ const authRoute = express.Router();
 
 authRoute.post("/register", userRegistration);
 authRoute.post("/login", userLogin);
+authRoute.post("/logout", authMiddleware, userLogout);
 authRoute.get("/me", authMiddleware, checkAuthentication);
 
 export default authRoute;
